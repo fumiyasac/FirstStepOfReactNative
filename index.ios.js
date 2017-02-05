4db0e48a43ec79a941f0ce73f09e05a539d741bd
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2017-02-05 23:35:16 +0900

    add comments in index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,26 @@
  * @flow
  */
 
+/**
+ * (次のToDo)アプリの雛形っぽいところまで作ってみる
+ * http://qiita.com/y_matsuwitter/items/a7fc88e566b80b4c1eea
+ */
+
+/**
+ * Reactのインポート文
+ *
+ * (参考)以前の書き方だとこのような形になっていた
+ * 'use strict';
+ * var React = require('react-native');
+ */
 import React, { Component } from 'react';
+
+/**
+ * 使うコンポーネント(配置したい部品)の定義
+ *
+ * → ReactNativeの公式ドキュメント内「COMPONENTS」から配置したいものを適宜引っ張ってくる形
+ * https://facebook.github.io/react-native/docs/getting-started.html
+ */
 import {
   AppRegistry,
   StyleSheet,
@@ -12,6 +31,12 @@ import {
   View
 } from 'react-native';
 
+/**
+ * コンポーネントに関する設定やスタイル適用をこの中で行う
+ *
+ * (ポイント) style=styles.[設定したプロパティ]の形でスタイルの適用を行う
+ * ※ 個人的な所感としてはAndroidのレイアウトに近い作成方法
+ */
 export default class FirstStepOfReactNative extends Component {
   render() {
     return (
@@ -31,6 +56,16 @@ export default class FirstStepOfReactNative extends Component {
   }
 }
 
+/**
+ * スタイルの適用
+ *
+ * (参考) ReactNativeのFlexboxレイアウト
+ * http://minotaur.badwitch.io/reactnative-flexbox/
+ * (参考) A Complete Guide to Flexbox
+ * https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+ *
+ * createメソッドの中にプロパティを設定しスタイルを定義する
+ */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -50,4 +85,5 @@ const styles = StyleSheet.create({
   },
 });
 
+//コンポーネントの適用を行って画面に表示する
 AppRegistry.registerComponent('FirstStepOfReactNative', () => FirstStepOfReactNative);
